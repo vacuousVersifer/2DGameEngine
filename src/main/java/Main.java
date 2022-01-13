baee93f7df7784133c6e6000da4f03552280edd0
davisbruce413 <davisbruce413@gmail.com>
2022-01-13 15:02:45 -0600

    1/13 School End (Making Asset Pool)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import jade.Window;
 
 /*
       From the tutorial located at
-      https://youtu.be/025QFeZfeyM?t=17727
+      https://youtu.be/025QFeZfeyM?t=18783
       (Update everytime used)
  */
 
--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -5,6 +5,7 @@ import org.joml.Vector2f;
 import org.joml.Vector4f;
 import renderer.Shader;
 import renderer.Texture;
+import util.AssetPool;
 
 public class LevelEditorScene extends Scene {
     public LevelEditorScene() {}
@@ -32,11 +33,17 @@ public class LevelEditorScene extends Scene {
                 this.addGameObjectToScene(go);
             }
         }
+
+        loadResources();
+    }
+
+    private void loadResources() {
+        AssetPool.getShader("assets/shaders/default.glsl");
     }
 
     @Override
     public void update(float dt) {
-        System.out.println("FPS: " + (1.0f / dt));
+        //System.out.println("FPS: " + (1.0f / dt));
         for(GameObject go : this.gameObjects) {
             go.update(dt);
         }
